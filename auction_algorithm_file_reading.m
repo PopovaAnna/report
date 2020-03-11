4e7b229e7a9f732856f11c6035cd4035b6e99e39
Popova Anna <popova.annushka@mail.ru>
2020-03-11 21:07:20 +0300

    changes from the tutor, input and output files, and an example for a nes solution

--- auction_algorithm_file_reading.m
+++ auction_algorithm_file_reading.m
@@ -1,8 +1,10 @@
+%ВСЕ ГОТОВО - НЕ ИСПРАВЛЯТЬ
 %решение задачи о назначениях (максимизация)
 %аукционный алгоритм для квадратной матрицы
-
+data="input6.txt";
+result="output6.txt";
 %ввод из файла матрицы стоимости 
-A = dlmread("input1.txt");
+A = dlmread(data)
 [n,n1] = size(A);%размерность
 C = A;
 %создание переменных
@@ -13,13 +15,21 @@ i2h = zeros(n,1);% вектор человек-предмет
 quantity = 0; 
 %количество назначений: quantity=n - условие окончания
 current = 1; %текуший человек
+iter=0;
 while (quantity!=n)
+  iter++;
+  if (iter>=n*5)
+    f = fopen(result, "w");
+    fprintf(f,"Looping: %s", data);
+    fclose(f);
+    dbquit
+   end
 i = current; %обрабатываем текущего человека
 [max_el,ix] = max(A(i,:)); %максимальный элемент и его индекс для i
 if (i2h(ix)==0)%предмет свободен
   s(i) = ix; %закрепляем этот предмет за i
   i2h(ix) = i; %предмет теперь занят
-  quantity = quantity+1;
+  quantity ++;
   current = quantity+1;%текущим становится человек следующий, за всеми просмотренными
   continue;
 endif
@@ -29,13 +39,13 @@ assi = i2h(ix); %кем занят
 if (v_assi >= v_i)
 %назначение не меняется у assi, меняется вектор цен и матрица
 %i-му ничего не отдаем
-p(ix) = p(ix)+v_assi;
+p(ix) += v_assi;
 A(:,ix) = A(:,ix) - v_assi;
 continue;
 endif
 %i выиграл спор
 %меняется вектор цен, предмет у assi-го отдаем i-му, меняем матрицу
-p(ix) = p(ix) + v_i; 
+p(ix) += v_i; 
 s(assi) = 0;
 s(i) = ix;
 i2h(ix) =i;
@@ -48,10 +58,12 @@ for i=1:n
   c_f += C(i,s(i));
 endfor
 %вывод
-f = fopen("output.txt", "w");
+f = fopen(result, "w");
+fprintf(f,"Good task: %s\n", data);
+fprintf(f,"Iteration =  %d\n", iter);
 fprintf(f,"Objective function = %d\n", c_f);
-fprintf(f,"Human <-> Object\n");
+fprintf(f,"Human <-> Object | Price\n");
 for l=1:n
-  fprintf(f,"%d <-> %d\n",l,s(l));
+  fprintf(f,"%d <-> %d |  %d\n",l,s(l),p(l));
 endfor
 fclose(f);
